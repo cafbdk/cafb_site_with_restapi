7891d00f3ad3a03829958ce40e57cd8fa5f36edb
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-03-05 15:55:38 -0500

    updated requirements

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,11 +3,18 @@ from django.views.generic import TemplateView, View
 # Create your views here.
 
 from .nutrition import UpcFood
+import os
 
+envs = {
+    'TOKEN' : os.environ.get('api_key', ''),
+    'ID': os.environ.get('api_id', ''),
+    }
+
+if '' in envs.values(): ### in one value isn't set
+    raise ImproperlyConfigured('landing_page.views: Set all values in the .env or with heroku:config.')
 
 class HomeView(TemplateView):
     template_name = 'landing_page.html'
-
 
 class UPCAPI(View):
     http_method_names = [u'post']
@@ -16,13 +23,36 @@ class UPCAPI(View):
         """
         Custom method for listview
         """
-        email = request.POST.get('upcCode', 'not works')
+        upc_code = request.POST.get('upcCode', 'not works')
 
+        api_key = os.environ.get('api_key', '')  #api_key, 
+        api_id = os.environ.get('api_id', '') #api_id)
         u = UpcFood(upc_code, api_key, api_id)
 
         context = u.convert_dict_to_attributes()
 
-        context.update({'item':'works'})
+        context.update({'upc_code':'ok'})
+
+        raise Exception(context)
+
+        return HttpResponse(json.dumps(context), content_type = "application/json") 
+
+    def post(self, request, *args, **kwargs):
+        """
+        Custom method for listview
+        """
+        upc_code = request.POST.get('upcCode', 'not works')
+
+        api_key = os.environ.get('api_key', '')  #api_key, 
+        api_id = os.environ.get('api_id', '') #api_id)
+        u = UpcFood(upc_code, api_key, api_id)
+
+        context = u.convert_dict_to_attributes()
+
+        context.update({'upc_code':'ok'})
+
+        raise Exception(context)
 
         return HttpResponse(json.dumps(context), content_type = "application/json")  
+
 
