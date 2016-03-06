457f426acd90e6670f0db46c54bc0acde29f01d5
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-03-06 00:16:10 -0500

    updated db int operations

--- restapi/models.py
+++ restapi/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    gtin_code = models.IntegerField()
+    gtin_code = models.TextField()
     gtin_name = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
