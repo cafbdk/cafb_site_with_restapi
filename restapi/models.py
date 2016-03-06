4cbd53c26ad493a9289b1e2233e936d51b06508c
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-03-05 21:16:38 -0500

    initial data loaded

--- restapi/models.py
+++ restapi/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    gtin_code = models.IntegerField()
+    gtin_name = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created',)
