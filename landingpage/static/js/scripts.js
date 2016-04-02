2ae03cc76841248a9d926ff6859a6deac924f9da
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2016-04-02 14:51:53 -0400

    WIP: Implement adding a wellness rule

--- landingpage/static/js/scripts.js
+++ landingpage/static/js/scripts.js
@@ -4,9 +4,54 @@
 $( document ).ready(function() {
       $(".button-collapse").sideNav();
 
+        $('#wellness_rule_form').submit(function(){
+            event.preventDefault();
+            // console.log("Wellness!"); 
+            var data = $(this).serialize();
+            // console.log(data);
+
+            $.ajax({
+                url : "wellness_rule/",
+                type : "POST",
+                // traditional: true,
+                data : post_data ,
+
+                // handle a successful response
+                success : function(data) { 
+                    console.log("success"); // another sanity check
+                    console.log(data);
+                    $('div[name="request_status"]').remove();
+                // if ok == true
+                    if (data['status'] == true){
+                        $('form#upc_ajax_form').parent().parent().append('   \
+                                <div name="request_status" class="divider"></div>     \
+                                  <div class="section">         \
+                                    <h4> UPC Requested </h4> \
+                                    <p class="flow-text"> \
+                                    Item Name: ' + data['gtin_name'] + '<br>' +
+                                    'UPC Code: '  + data['gtin_code'] +
+                                     '</p> </div> ');
+                    
+                        
+                    }
+                    else {
+                        $('form#upc_ajax_form').parent().parent().append('\
+                                <div name="request_status" class="divider"></div>     \
+                                  <div class="section">         \
+                                    <h4> UPC Not Found (maybe too many requests) </h4> \
+                                    <p class="flow-text"> \
+                                    UPC Code: '  + data['gtin_code'] + "</p> </div>");
+                    }
+
+                },
+                error : function (request, status, error) {
+                    console.log(request.responseText);
+                }
+            });
+        });
 
 
-        $('form#ajax_form').submit(function (e) { 
+        $('form#upc_ajax_form').submit(function (e) { 
             event.preventDefault();
             console.log("form submitted!"); 
 
@@ -25,46 +70,46 @@ $( document ).ready(function() {
 
         });
 
-            function create_post( post_data ) {
-            console.log("create post is working!") // sanity check
-            $.ajax({
-                url : "upc/", // the endpoint
-                type : "POST", // http method
-                traditional: true,
-                data : post_data , // data sent with the post request
+        function create_post( post_data ) {
+        console.log("create post is working!") // sanity check
+        $.ajax({
+            url : "upc/", // the endpoint
+            type : "POST", // http method
+            traditional: true,
+            data : post_data , // data sent with the post request
 
-                // handle a successful response
-                success : function(data) { 
-                    console.log("success"); // another sanity check
-                    console.log(data);
-                    $('div[name="request_status"]').remove();
-                // if ok == true
-                    if (data['status'] == true){
-                        $('form#ajax_form').parent().parent().append('   \
-                                <div name="request_status" class="divider"></div>     \
-                                  <div class="section">         \
-                                    <h4> UPC Requested </h4> \
-                                    <p class="flow-text"> \
-                                    Item Name: ' + data['gtin_name'] + '<br>' +
-                                    'UPC Code: '  + data['gtin_code'] +
-                                     '</p> </div> ');
+            // handle a successful response
+            success : function(data) { 
+                console.log("success"); // another sanity check
+                console.log(data);
+                $('div[name="request_status"]').remove();
+            // if ok == true
+                if (data['status'] == true){
+                    $('form#upc_ajax_form').parent().parent().append('   \
+                            <div name="request_status" class="divider"></div>     \
+                              <div class="section">         \
+                                <h4> UPC Requested </h4> \
+                                <p class="flow-text"> \
+                                Item Name: ' + data['gtin_name'] + '<br>' +
+                                'UPC Code: '  + data['gtin_code'] +
+                                 '</p> </div> ');
+                
                     
-                        
-                    }
-                    else {
-                        $('form#ajax_form').parent().parent().append('\
-                                <div name="request_status" class="divider"></div>     \
-                                  <div class="section">         \
-                                    <h4> UPC Not Found (maybe too many requests) </h4> \
-                                    <p class="flow-text"> \
-                                    UPC Code: '  + data['gtin_code'] + "</p> </div>");
-                    }
-
-                },
-                error : function (request, status, error) {
-                    console.log(request.responseText);
                 }
-            });
-        }
+                else {
+                    $('form#upc_ajax_form').parent().parent().append('\
+                            <div name="request_status" class="divider"></div>     \
+                              <div class="section">         \
+                                <h4> UPC Not Found (maybe too many requests) </h4> \
+                                <p class="flow-text"> \
+                                UPC Code: '  + data['gtin_code'] + "</p> </div>");
+                }
+
+            },
+            error : function (request, status, error) {
+                console.log(request.responseText);
+            }
+        });
+    }
 
 });
